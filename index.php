4f24049f1c142951e26ee84707e50d9ca46d6ce0
kevin <skelev@mbp-de-kevin.home>
2018-12-03 22:16:28 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>Elysées Hongkong</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Elysées Hongkong</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Réservation<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">La Carte</a>
+            <a class="nav-item nav-link" href="#">Contact</a>
+            <a class="nav-item nav-link" href="#">Creer un compte</a>
+
+
+            <p> 01 46 51 60 99</p>
+        </div>
+
+    </div>
+</nav>
+
+    <header>
+
+        <h1>Bienvenue</h1>
+        <h2>Cuisine chinoise et thailandaise</h2>
+        <p>
+            Venez découvrir ou redécouvrir une cuisine asiatique aux spécialités vietnamiennes et chinoises. Le
+            restaurant vous
+            propose de nombreux plats traditionnels cuisinés uniquement sur commande. Les produits sont d'une véritable
+            fraicheur et les plats d'une saveur authentique.
+        </p>
+
+    </header>
+
+
+</body>
+<script type="text/javascript" src="assets/js/script.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</html>
